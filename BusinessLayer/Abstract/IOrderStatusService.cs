407e3826d4c61536eac1875eb924df96c3165246
Taha Hasemi <kiziltahaaslan@gmail.com>
2021-10-14 12:06:28 +0330

    DataAccessLayer and BusinessLayer

---
+++ BusinessLayer/Abstract/IOrderStatusService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IOrderStatusService
+    {
+        void AddOrderStatus(OrderStatus orderStatus);
+        void DeleteOrderStatus(OrderStatus orderStatus);
+        void UpdateOrderStatus(OrderStatus orderStatus);
+        List<OrderStatus> GetList();
+        OrderStatus GetByID(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/BillManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BillManager : IBillService
+    {
+        public void AddBill(Bill bill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBill(Bill bill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bill GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Bill> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBill(Bill bill)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
