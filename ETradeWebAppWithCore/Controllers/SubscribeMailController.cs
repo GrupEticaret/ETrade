cda5140d1ea26274163649f7581541b76a04acf6
onuralp aslan <aslanonura@gmail.com>
2021-11-07 19:58:20 +0300

    dinamik subscribe

---
+++ ETradeWebAppWithCore/Controllers/SubscribeMailController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using ETradeWebAppWithCore.Models.AraModeller;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETradeWebAppWithCore.Controllers
+{
+    public class SubscribeMailController : Controller
+    {
+        SubscribeMailManager sm = new SubscribeMailManager(new EfSubscribeMailRepository());
+        public async Task<IActionResult> SubscribeMail(SubscribeMail p)
+        {
+            try
+            {
+                p.MailStatus = true;
+                await sm.AddSubscribeMail(p); ;
+
+                return Json(new ResultModel { Success = true });
+            }
+            catch (System.Exception ex)
+            {
+                return Json(new ResultModel { Success = false, Message = ex.Message });
+            }
+
+
+
+
+        }
+    }
+}
