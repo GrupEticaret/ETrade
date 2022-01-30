5c318f56784ba1288325bddc03f4205812a135c7
iremsmr <iremsamur129@gmail.com>
2022-01-30 19:02:51 +0300

    updated

---
+++ BusinessLayer/ValidationRules/UserValidator.cs
@@ -0,0 +1,42 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        
+        public UserValidator()
+        {
+            Regex regex = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@!%*?+#&'()[=\"€])[A-Za-z\\d$@!%*?+#&'()[=\"€']{8,}");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Yazar adı soyadı alanı doldurulmalıdır!!");
+            RuleFor(x => x.UserTcIdentificationNumber).NotEmpty().WithMessage("Tc Kimlik alanı doldurulmalıdır.");
+            RuleFor(x => x.UserEmail).NotEmpty().WithMessage("Email alanı doldurulmalıdır.");
+            RuleFor(x => x.UserMobileNumber).NotEmpty().WithMessage("Email alanı doldurulmalıdır.");
+            RuleFor(x => x.UserPassword).NotEmpty().WithMessage("Email alanı doldurulmalıdır.");
+            RuleFor(x => x.UserName).MinimumLength(2).WithMessage("Lütfen en az 2 karakterlik veri girişi yapın!!");
+            RuleFor(x => x.UserName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterlik veri girişi yapın!!");
+            RuleFor(x => x.UserPassword).Matches(regex).WithMessage("Şifre en az bir küçük harf, bir büyük harf, 1 özel karakter ve 1 rakam içermelidir. Ve en az 8 karakter olmalıdır!!");
+            RuleFor(x => x.UserEmail).EmailAddress().WithMessage("Lütfen doğru bir formatta mail adresinizi girin.");
+    
+            RuleFor(x => x.UserTcIdentificationNumber).Length(11).WithMessage("Tc Kimlik numarası 11 haneli olmalıdır.");
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+        
+
---
+++ ETradeWebAppWithCore/Controllers/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using FluentValidation.Results;
+
+namespace ETradeWebAppWithCore.Controllers
+{
+    public class RegisterController : Controller
+    {
+        UserManager userManager = new UserManager(new EfUserRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            
+            
+        
+            UserValidator userValidator = new UserValidator();
+            DateTime nowDate =  DateTime.Now; // sadece tarih
+
+            ValidationResult results = userValidator.Validate(user); 
+            if (results.IsValid)//Eğer sonuçlar geçerli ise
+            {
+
+                user.UserRegistrationDate = nowDate;
+                userManager.TAdd(user);
+                ViewBag.regiterOperationControl = true;
+                return RedirectToAction("Register", "Register");
+            }
+            else
+            {
+                
+
+                foreach (var item in results.Errors)
+                {
+
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+
+            return View();//işlem geçersiz ise bana bir view döndürür.
+            
+
+        }
+    }
+}
